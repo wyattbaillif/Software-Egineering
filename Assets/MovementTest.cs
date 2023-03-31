af9bd5b726fc9ee4585d9e6d0813b0e14f61cd84
g-harinen <64097842+g-harinen@users.noreply.github.com>
2023-03-30 22:41:27 -0500

    Fixed movement
    Made it so that arrow keys and wasd move the player in equal amounts for all directions

--- Assets/MovementTest.cs
+++ Assets/MovementTest.cs
@@ -7,8 +7,6 @@ public class MovementTest : MonoBehaviour
     //creating public variables add them to unity's UI -Wyatt
 
     public float speed =.05f;
-    public float jump = 1f;
-
 
     // Update is called once per frame
     void FixedUpdate()
@@ -16,7 +14,7 @@ public class MovementTest : MonoBehaviour
         float xDirection = Input.GetAxis("Horizontal");
         float yDirection = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(xDirection, yDirection*jump, 0.0f);
+        Vector3 moveDirection = new Vector3(xDirection, yDirection, 0.0f);
 
         transform.position += moveDirection * speed;
 

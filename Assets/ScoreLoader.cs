318ba15969a8a57e405a1c310e4d136fb0df9928
Keegan-Cox <Keegan.Cox+SoftwareEngineering@go.mnstate.edu>
2023-04-27 14:07:54 -0500

    Added query to local database.
    I will work on getting this in the cloud so you all can use it.

--- Assets/ScoreLoader.cs
+++ Assets/ScoreLoader.cs
@@ -18,15 +18,52 @@ public class ScoreLoader : MonoBehaviour
 			{
 				Debug.LogError("Error: " + webRequest.error);
 			}
-			else
-			{
-				Debug.Log("Fetched string: " + webRequest.downloadHandler.text);
-			}
-		}
-	}
-	// Update is called once per frame
-	void Update()
-    {
-        
+            else
+            {
+                List<UserScore> userScores = new List<UserScore>();
+                string responseText = webRequest.downloadHandler.text;
+
+                // Split the response string into individual user-score strings
+                string[] userScoreStrings = responseText.Split(';');
+
+                // Parse each user-score string and add to the list
+                foreach (string userScoreString in userScoreStrings)
+                {
+                    if (userScoreString.Length > 0)
+                    {
+                        string[] keyValue = userScoreString.Split('|');
+                        if (keyValue.Length == 2)
+                        {
+                            string userName = keyValue[0].Split(':')[1];
+                            int score = int.Parse(keyValue[1].Split(':')[1]);
+                            UserScore userScore = new UserScore(userName, score);
+                            userScores.Add(userScore);
+                        }
+                    }
+                }
+
+                // Sort the list in descending order based on the score
+                userScores.Sort((a, b) => b.score.CompareTo(a.score));
+
+                // Log the top three scores and their associated user names
+                for (int i = 0; i < 3 && i < userScores.Count; i++)
+                {
+                    Debug.Log("User " + userScores[i].userName + " scored " + userScores[i].score);
+                }
+            }
+        }
     }
 }
+
+public class UserScore
+{
+	public string userName;
+	public int score;
+
+	public UserScore(string name, int scr)
+	{
+		userName = name;
+	    score = scr;
+	}
+}
+
